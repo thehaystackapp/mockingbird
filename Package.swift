a1213cfa6d44f1357f0ac41d9f562627b0c4bce1
Nicholas Lythall <nic.lyth@gmail.com>
2024-02-12 10:49:18 +1000

    Bump XcodeProj version
    Disable release signing

--- Package.swift
+++ Package.swift
@@ -58,7 +58,7 @@ if ProcessInfo.processInfo.environment["MKB_BUILD_EXECUTABLES"] != "1" {
       .package(url: "https://github.com/kylef/PathKit.git", .exact("1.0.1")),
       .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax.git", .exact("0.50500.0")),
       .package(url: "https://github.com/jpsim/SourceKitten.git", .exact("0.30.0")),
-      .package(url: "https://github.com/tuist/XcodeProj.git", .exact("8.7.1")),
+      .package(url: "https://github.com/tuist/XcodeProj.git", .exact("8.18.0")),
       .package(url: "https://github.com/weichsel/ZIPFoundation.git", .exact("0.9.14")),
     ],
     targets: [
